c8e9a19ed24c7a0bad006b7aedb6cb55cc8b3d74
Ilyas Ilyasov <38839317+cawabungag@users.noreply.github.com>
2023-04-02 02:41:02 +0300

    delete world root

--- Assets/Scripts/Installers/GameSceneInstaller.cs
+++ Assets/Scripts/Installers/GameSceneInstaller.cs
@@ -1,19 +1,14 @@
-using DefaultNamespace;
 using Game.Services.MapGenerator;
 using Game.Services.MapGenerator.Impls;
-using UnityEngine;
 using Zenject;
 
 namespace Installers
 {
 	public class GameSceneInstaller : MonoInstaller
 	{
-		[SerializeField]
-		private WorldRoot _worldRoot;
 		public override void InstallBindings()
 		{
 			Container.Bind<IMapGenerator>().To<MapGenerator>().AsSingle();
-			Container.BindInstance(_worldRoot);
 		}
 	}
 }
